06976bbcf59557f6a0c411f5659b6ebb0599e522
Carl Wainwright <carljame7@gmail.com>
2018-07-31 17:17:12 +0100

    rewrote code

--- Curious Katie/ViewController/ViewController.swift
+++ Curious Katie/ViewController/ViewController.swift
@@ -9,185 +9,143 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
+    
+    //create empty array for participants
+    var participants: [Person] = []
+    //Random number generator between 2 and 12
+    let participantCount = Int(arc4random_uniform(10))+2
+    //pull viewModel through to be used in this file
+    let viewModel = ViewModel()
+    //currentParticipant is equat to an object from Person
+    var currentParticipant: Person!
     
     
-    //Declares hobby is the name array from Interest file
-    var hobby = Interest.generateGeneralHobbies()
-    //pull list of random participants through
-    var participants = Person.generateParticipants(amount: Int(arc4random_uniform(10)) + 2)
-    //declare value for participantCoutIncrease
-    var participantCountIncrease = 0
-    var valueIncrease = 0
-//    //Create empty Dctionary with Array
-//    var interestDictionary = [String: [String]]()
-    //declares data used in UIPickerView
-    var interestsData = [String]()
-    var newInterestName = ""
-    var currentPersonAvailableInterests: [String] = []
-    var randParticipants: [String] = []
     
     
     @IBOutlet weak var interestsPickerView: UIPickerView!
     @IBOutlet weak var showParticipants: UIButton!
-    @IBOutlet weak var playerList: UIButton!
-    @IBOutlet weak var startAddingInterests: UIButton!
-    @IBOutlet weak var addInterest: UIButton!
+    @IBOutlet weak var compareInterests: UIButton!
+    @IBOutlet weak var addInterests: UIButton!
     @IBOutlet weak var noMoreInterests: UIButton!
+    @IBOutlet weak var participantLabel: UILabel!
+    
+    
     
     @IBAction func showParticipants(_ sender: UIButton) {
         
         //Print name and attributes of all participants
         participants.forEach { (participant) in
-            print("My name is \(participant.name), I am \(participant.gender.rawValue) aged \(participant.age) and I live in \(participant.city ?? "Paris")")
-            
-            showParticipants.disableButton()
-            startAddingInterests.enableButton()
-        }
-    }
-
-    @IBAction func playerList(_ sender: UIButton) {
-
-        participants.forEach{ (participant) in
-            print("My name is \(participant.name) \(participant.finishedAddingInterests)")
-                participant.interests.forEach{ (interest) in
-                print ("I like \(interest.name), which is \(interest.description). And this requires \(interest.requiredEquipment)")
-                }
-        }
-        valueIncrease += 1
-        
-        
-
-    }
-    
-    
-    @IBAction func startAddingInterests(_ sender: UIButton) {
-        
-        
-        
-        print("\(participants[participantCountIncrease].name) please select your interests from the list.")
-        
-        startAddingInterests.disableButton()
-        addInterest.enableButton()
-
-        
-        interestsPickerView.isHidden = false
-        
-        
-        
-    }
-    
-    @IBAction func addInterest(_ sender: UIButton) {
-        
-       
-        //*************filters participants, but removes them - change to if participant filter is true then continue or next participant
-
-
-
-        //filtere participants to variable of participants with finishedAddingInteretsts as false
-        let randParticipants = participants.filter {$0.finishedAddingInterests == false}
-        let finishedAddingForParticipants = participants.filter {$0.finishedAddingInterests == true}
-        //change participantCountIncrease to be a random number upto the filtered participants
-        participantCountIncrease = Int(arc4random_uniform(UInt32(randParticipants.count)))
-        
-        //if all participants have finishedAddingInterests as True then
-        if finishedAddingForParticipants.count == participants.count{
-            interestsPickerView.isHidden = true
-            addInterest.disableButton()
-            noMoreInterests.disableButton()
-        }
-        else {
-
-        
-        func getAvailableInterests(of participant: Person) {
-            
-            let set1: Set<String> = Set(participant.interests.map { $0.name })
-
-            let set2: Set<String> = Set(hobby.map{ $0.name})
-            
-            currentPersonAvailableInterests = Array(set2.subtracting(set1))
-
+            print ("My name is \(participant.name), I am \(participant.gender.rawValue) aged \(participant.age) and I live in \(participant.city ?? "Paris")")
         }
         
-        interestsData = currentPersonAvailableInterests
+        nextParticipant()
+        showParticipants.disableButton()
         
-
-        //reload pickerview after the prior removal above
-        interestsPickerView.delegate = self
-        
-        //Reloads the pickerview to show all interests
         interestsPickerView.reloadAllComponents()
-        
-        if let myNewInterest = (hobby.filter{ $0.name == newInterestName }).first {
-            participants.first { $0.name == participants[participantCountIncrease].name }?.interests.append(myNewInterest)}
-        
-
-        
-
-        print("\(randParticipants[participantCountIncrease].name) please select your interests from the list.")
-        
-
-
-        playerList.enableButton()
-        noMoreInterests.enableButton()
+        //participantList.forEach { (participant) in
+        //            print("My name is \(participant.name), I am \(participant.gender.rawValue) aged \(participant.age) and I live in \(participant.city ?? "Paris")")
+        //
+        //            showParticipants.disableButton()
+        //            startAddingInterests.enableButton()
+        //
+        //        }
         
     }
+    
+    
+    @IBAction func addInterests(_ sender: UIButton) {
+        
+        
+        
+        viewModel.updateParticipant(person: currentParticipant, within: participants, with: Interest(name: viewModel.currentPersonAvailableInterests[interestsPickerView.selectedRow(inComponent: 0)], description: "", requiredEquipment: ""))
+        
+        nextParticipant()
+        
+        interestsPickerView.reloadAllComponents()
+        //        if let myNewInterest = (hobby.filter{ $0.name == newInterestName }).first {
+        //            participants.first { $0.name == participants[participantCountIncrease].name }?.interests.append(myNewInterest)}
+        
+        
+        //print("\(participants[participantCountIncrease].name) please select your interests from the list.")
+        
+        //        startAddingInterests.disableButton()
+        //        addInterest.enableButton()
+        //
+        //        interestsPickerView.isHidden = false
+        
+        
+        
     }
+    
+    
     
     @IBAction func noMoreInterests(_ sender: UIButton) {
         
-        //Trying to find way of amenind finishedAddingInterest falg as true for participant
-        participants[participantCountIncrease].finishedAddingInterests = true
         
-        let randParticipants = participants.filter {$0.finishedAddingInterests == false}
-        participantCountIncrease = Int(arc4random_uniform(UInt32(randParticipants.count)))
-        print("\(randParticipants[participantCountIncrease].name) please select your interests from the list.")
- 
+        viewModel.hasFinished(person: currentParticipant, within: participants)
+        nextParticipant()
+        interestsPickerView.reloadAllComponents()
         
-//
-//            addInterest.disableButton()
-////            noMoreInterests.disableButton()
-            playerList.enableButton()
-//            interestsPickerView.isHidden = true
-        
-        
-        
-
+        compareInterests.enableButton()
         
     }
     
+    
+    
+    
+    @IBAction func compareInterests(_ sender: UIButton) {
+        
+        participants.forEach{ (participant) in
+            print("\n My name is \(participant.name)")
+            participant.interests.forEach{ (interest) in
+                print ("I like \(interest.name), which is \(interest.description). And this requires \(interest.requiredEquipment)")
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
-        interestsData = hobby.map({$0.name})
+        participants = Person.generateParticipants(amount: participantCount)
+        //        interestsData = hobby.map({$0.name})
         interestsPickerView.dataSource = self
         interestsPickerView.delegate = self
         
-        
-        playerList.disableButton()
-        startAddingInterests.disableButton()
-        addInterest.disableButton()
-        noMoreInterests.disableButton()
-        
-        interestsPickerView.isHidden = true
+        participantLabel.text = ""
         
         
         
     }
+    
+    func nextParticipant() {
+        
+        //
+        viewModel.getRandomPerson(participants: participants, onSuccess: { (name) in currentParticipant = name
+            
+            participantLabel.text = ("\(currentParticipant.name) choose your next interest.")
+        })
+        {
+            participantLabel.text = "Selections Complete"
+            interestsPickerView.isHidden = true
+            
+        }
+    }
+    
+    
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return interestsData.count
+        return viewModel.currentPersonAvailableInterests.count
     }
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return interestsData[row]
+        return viewModel.currentPersonAvailableInterests[row]
         
     }
-
+    
+    
 }
+
